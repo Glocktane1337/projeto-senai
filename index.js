22240e0ca4a87f560b4cf35e3e818906d3a57399
Glocktane1337 <gsouza.gs617@gmail.com>
2021-09-15 22:30:45 -0300

    tryna index this

--- index.js
+++ index.js
@@ -7,38 +7,56 @@ var data_evento = (22)
 
 var data_atual = window.prompt("Qual é o dia de hoje?")
 
+//Pede a data para o usuário.
+
+var dia_ok = false
+
+var idade_ok = false
+
+var idade_data_certas = false
+
+var inscritos_anteriores = 50
+
+var inscritos_max = 100
+
+var idade_minima = 18
+
+//declara as variáveis de confirmação como falsas por padrão.
+
 if (data_evento > data_atual) {
     var dia_ok = true 
-}
-    else {
-        var dia_ok = false
-        window.alert("Cadastro invalidado pela data. Clique em OK para encerrar o programa.")
-}
-//pede a data para o usuário e verifica se 
-//ela está conforme com a data do evento.
+    if (dia_ok = true) {
+        var idade_usuario = window.prompt("Qual é a sua idade?")
 
-if (dia_ok = false) {
-   var prosseguir1 = false;
-} else {
-    var prosseguir1 = true;
+            if (idade_usuario >= idade_minima) {
+                idade_ok = true
+                idade_data_certas = true }  
+                 else {
+                        alert("Idade inválida!")
+                        idade_data_certas = false}   
+                            if (idade_data_certas = true) {    
+                                alert("Idade e data de inscrição válidas!");
+                                alert("Verificando base de dados para cadastros anteriores...")} 
+                                    else {
+                                        alert("Cadastro inválido.")}              
+                                                                //A esse ponto, o script iria procurar na database a quantidade de inscrições anteriores,
+                                                                //mas como não sei esse comando, vou considerar 50 participantes já inscritos.
+                        if (inscritos_anteriores < inscritos_max ) {
+                            alert("Inscrição feita com sucesso!")
+                            alert("O evento será dia 22 de Setembro, não falte!")}
+                                else {
+                                    alert("Cadastro invalidado, quantidade limite de participantes já atingida.")
+                                }
+                            }
+                              
 }
-//impede o programa de continuar caso a data seja inválida.
+else {
+    alert("Data inválida!")
+    idade_data_certas = false }
 
-if (prosseguir1 = true) {
-var idade_usuario = window.prompt("Qual é a sua idade?")
-var idade_minima = 18
-if (idade_usuario >= idade_minima) {
-    var prosseguir2 = true;
-        } else {
-            var prosseguir2 = false
-        }
-    }
-//impede o programa de continuar caso a idade seja insuficiente.
 
-if (prosseguir1 = true , prosseguir2 = true) {
-    alert("Idade e data de inscrição válidas!")
-    alert("Verificando base de dados para cadastros anteriores...")
-}
+//ninho de if/elses verificando a conformância da idade e da data,
+//encerra o script caso alguma delas não esteja confrome.
 
  
 
